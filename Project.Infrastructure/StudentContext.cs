ef6fd44b3f4eca6897c47839783663903fdd67bd
CristinaHoamea <cristina.hoamea@gmail.com>
2016-12-14 22:16:30 +0200

    Add files via upload

---
+++ Project.Infrastructure/StudentContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.core;
+
+namespace Project.Infrastructure
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EntityModel;Trusted_Connection=True;");
+        }
+    }
+}
