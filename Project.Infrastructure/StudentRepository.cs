b9ea632806eaa23ba2bba7881df1b6bc0d68c632
CristinaHoamea <cristina.hoamea@gmail.com>
2016-12-14 20:24:41 +0200

    Add files via upload

---
+++ Project.Infrastructure/StudentRepository.cs
@@ -0,0 +1,37 @@
+﻿using Project.core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Project.core;
+
+namespace Project.Infrastructure
+{
+    public class StudentRepository : IStudentRepository
+    {
+        public void Add(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student FindById(string studentID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int studentID)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Project.Infrastructure/TeacherRepository.cs
@@ -0,0 +1,44 @@
+﻿using Project.core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Project.core;
+
+namespace Project.Infrastructure
+{
+    public class TeacherRepository : ITeacherRepository
+    {
+        TeacherContext context = new TeacherContext();
+        public void Add(Teacher teacher)
+        {
+            context.Teachers.Add(teacher);
+        }
+
+        public void Edit(Teacher teacher)
+        {
+            var toUpdate = context.Teachers.FirstOrDefault(t => t.TeacherId == teacher.TeacherId);
+            toUpdate.TeacherFirstName = teacher.TeacherFirstName;
+            toUpdate.TeacherLastName = teacher.TeacherLastName;
+            context.SaveChanges();
+        }
+
+        public Teacher FindById(int teacherID)
+        {
+            var teacher = (from t in context.Teachers where t.TeacherId.Equals(teacherID) select t).FirstOrDefault();
+            return teacher;
+
+        }
+
+        public IEnumerable<Teacher> GetTeachers()
+        {
+            return context.Teachers;
+        }
+
+        public void Remove(int teacherID)
+        {
+            Teacher teacher = (from t in context.Teachers where t.TeacherId.Equals(teacherID) select t).FirstOrDefault();
+            context.Teachers.Remove(teacher);
+            context.SaveChanges();
+        }
+    }
+}
