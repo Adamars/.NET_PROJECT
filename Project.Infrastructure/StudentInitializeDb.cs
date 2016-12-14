dbfccb4718478d0ca0a8a4fbdefdd02b626af70a
CristinaHoamea <cristina.hoamea@gmail.com>
2016-12-14 22:17:01 +0200

    Add files via upload

---
+++ Project.Infrastructure/StudentInitializeDb.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project.core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.Infrastructure
+{
+    public class StudentInitializeDb : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>().Property(student => student.StudentFirstName).
+                IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Student>().Property(student => student.StudentLastName).
+               IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<Student>().Property(student => student.MeanA1).
+               IsRequired();
+            modelBuilder.Entity<Student>().Property(student => student.MeanA2).
+              IsRequired();
+            modelBuilder.Entity<Student>().Property(student => student.Preferences).
+              IsRequired().HasMaxLength(100);
+        }
+    }
+}
